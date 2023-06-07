d13575be32b5ccbe150552d4d4b9db0d1dc2797d
Ulqiora <damdinovandrey@gmail.com>
2023-06-07 22:01:20 +0700

    obsercer is ready creating

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <thread>
-#include "ReportComponent.h"
+#include "model/AgentReportF/ReportComponent.h"
 #include <vector>
 using namespace std;
 
 int main() {
-    ReportComponent* report = new ReportComposite();
-    report->add(new ReportValue("Processor state",59));
-    report->add(new ReportValue("RAM",8096));
+    ReportComposite* report = new ReportComposite("Root",nullptr);
+    report->add(new ReportValue("Processor state",59,report));
+    report->add(new ReportValue("RAM",8096,report));
 }
--- src/model/AgentReportF/ReportComponent.h
+++ src/model/AgentReportF/ReportComponent.h
@@ -1,37 +1,49 @@
 #pragma once
+#include <iostream>
+#include <list>
 #include <string>
 #include <vector>
+#include "Observer.h"
 
-class ReportComponent{
-    public:
-    ReportComponent(std::string namecomponent):name_(namecomponent){}
-    virtual bool add(ReportComponent* ) = 0;
-    virtual bool updateValue(double value) = 0;
-    virtual std::string getname() = 0;
-    private:
+class ReportComponent {
+ public:
+    ReportComponent(std::string namecomponent, ReportComponent* parent)
+        : name_(namecomponent), parent_(parent) {}
+    virtual void Accept(IReportObserver* visitor) = 0;
+    std::string_view GetName(){return name_;}
+    ReportComponent* GetParent(){return parent_;}
+    virtual ~ReportComponent() = default;
+ private:
     std::string name_;
+    ReportComponent* parent_;
 };
 
-class ReportComposite: public ReportComponent{
-    public:
-    ReportComposite(std::string namecomponent):ReportComponent(namecomponent){}
-    bool add(ReportComponent* component) {
-        components_.push_back(component);
-        return true;
+class ReportComposite : public ReportComponent {
+ public:
+    ReportComposite(std::string namecomponent, ReportComponent* parent)
+        : ReportComponent(namecomponent, parent) {}
+    void add(ReportComponent* component) {
+        if (component != nullptr) components_.push_back(component);
     }
-    bool updateValue(double value) override {return false;};
-    private:
+    size_t Size() { return components_.size(); }
+    ReportComponent* GetNode(size_t i){return components_[i];}
+    void Accept(IReportObserver* visitor) override {
+        visitor->Visit(this);
+    }
+    virtual ~ReportComposite() = default;
+ private:
     std::vector<ReportComponent*> components_;
-    
 };
-class ReportValue: public ReportComponent{
-    public:
-    ReportValue(const std::string& nameMetric, double value): ReportComponent(nameMetric),value_(value){}
-    bool add(ReportComponent* component) override { return false; }
-    bool updateValue(double value) override {
-        value_ = value;
-        return true;
+class ReportValue : public ReportComponent {
+ public:
+    ReportValue(const std::string& nameMetric, double value, ReportComponent* parent)
+        : ReportComponent(nameMetric, parent), value_(value) {}
+    void updateValue(double value) { value_ = value; }
+    virtual ~ReportValue() = default;
+    void Accept(IReportObserver* visitor)override{
+        visitor->Visit(this);
     }
-   private:
+ private:
     double value_;
 };
+

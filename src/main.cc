2256752fa1e45c2d0f14af968c803dededab49b2
Ulqiora <damdinovandrey@gmail.com>
2023-06-17 22:15:54 +0700

    не понимаю как посчитать пропуск.  способ. macos

--- src/main.cc
+++ src/main.cc
@@ -1,91 +1,84 @@
-// #include <cstdio>
-// #include <fstream>
-// #include <iostream>
-// #include <string>
-// #include <thread>
-// #include <vector>
-// // #include <sys/param.h>
-// // #include <sys/mount.h>
-// #include <unistd.h>
-// #include <sys/stat.h>
+#include <sys/mount.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
 // #include <sys/vfs.h>
-// #include <sys/types.h>
+#include <sys/types.h>
 // #include <sys/sysinfo.h>
-// // #include "libs/json/single_include/nlohmann/json.hpp"
-// // #include "model/AgentReportF/Builder.h"
-// // #include "model/AgentReportF/ReportComponent.h"
-// // #include "model/Agents/CPU/CpuAgent.h"
-// using namespace std;
-// // using json = nlohmann::json;
-// int main() {
-//     int num_processes = sysconf(_SC_NPROCESSORS_ONLN);
-//     std::cout << "Number of processes: " << num_processes << std::endl;
-//         struct sysinfo si;
-//     if (sysinfo(&si) == 0) {
-//         std::cout << "Total RAM: " << si.totalram / 1024 / 1024 << " MB" << std::endl;
-//     }
-//     if (sysinfo(&si) == 0) {
-//         double used_mem = si.totalram - si.freeram;
-//         double mem_percent = used_mem / si.totalram * 100.0;
-//         cout << "Memory usage: " << mem_percent << "%" << endl;
-//     }
+#include "libs/json/single_include/nlohmann/json.hpp"
+#include "model/AgentReportF/Builder.h"
+#include "model/AgentReportF/ReportComponent.h"
+#include "model/Agents/CPU/CpuAgent.h"
+#include "model/Agents/Memory/MemoryAgent.h"
+using namespace std;
+// using json = nlohmann::json;
+int main() {
+    char command[]="iostat -w 1 -c 4 disk0 ";
+    FILE* file = popen(command, "r");
+    if (!file) throw std::runtime_error("popen() failed!");
+    char buffer[128];
+    char* u;
+    while (!feof(file)) {
+        std::fgets(buffer, 128, file);
+        std::cout << buffer;
+    }
+    // (*iter_).second.setValue(strtod(buffer + 0, &u)), iter_.Next();
+    // pclose(file);
+    // struct statfs fs_info;
+    // int error = statfs("/", &fs_info);
 
+    // if (error != 0) {
+    //     std::cerr << "Error: " << strerror(errno) << std::endl;
+    //     return 1;
+    // }
 
+    // double disk_size = static_cast<double>(fs_info.f_bsize * fs_info.f_blocks) / (1024 * 1024 * 1024); //
+    // размер диска в Гб double free_space = static_cast<double>(fs_info.f_bsize * fs_info.f_bfree) / (1024 *
+    // 1024 * 1024); // свободное место в Гб double used_space = disk_size - free_space; // занятое место в
+    // Гб double io_time = 1.0 / (fs_info.f_iosize / 1000.0 / 1000.0); // время на I/O операции в секундах
+    // double max_throughput = used_space / io_time; // максимальная пропускная способность в Гб/с
 
+    // std::cout << "Disk size: " << disk_size << " GB";
+    // std::cout << "Free space: " << free_space << " GB";
+    // std::cout << "Used space: " << used_space << " GB";
+    // std::cout << "Maximum throughput: " << max_throughput << " GB/s";
 
+    // return 0;
+    // /* Закрываем поток */
+    // fs::path path = fs::current_path()/"example.json";
+    // IBuilder* bd = new Builder(path);
+    // IAgent* agent=new MemoryAgent(bd);
+    // agent->update();
+    // agent->update();
+    // for (ReportComposite::Iterator iter(agent->GetReport()); !iter.isEnd(); iter.Next()) {
+    //     auto report_i = (*iter);
+    //     for (auto& point : report_i.first) {
+    //         std::cout << point << std::endl;
+    //     }
+    //     std::cout << "value = "<<report_i.second.getValue() << std::endl;
+    //     std::cout << "critical value = "<< report_i.second.getCriticalValue() << std::endl;
+    //     std::cout << "Result compare = "<< report_i.second.compareIsTrue() << std::endl;
+    // }
 
-
-
-
-
-//     //     struct statfs fs_info;
-//     // int error = statfs("/", &fs_info);
-
-//     // if (error != 0) {
-//     //     std::cerr << "Error: " << strerror(errno) << std::endl;
-//     //     return 1;
-//     // }
-
-//     // double disk_size = static_cast<double>(fs_info.f_bsize * fs_info.f_blocks) / (1024 * 1024 * 1024); // размер диска в Гб
-//     // double free_space = static_cast<double>(fs_info.f_bsize * fs_info.f_bfree) / (1024 * 1024 * 1024); // свободное место в Гб
-//     // double used_space = disk_size - free_space; // занятое место в Гб
-//     // double io_time = 1.0 / (fs_info.f_iosize / 1000.0 / 1000.0); // время на I/O операции в секундах
-//     // double max_throughput = used_space / io_time; // максимальная пропускная способность в Гб/с
-
-//     // std::cout << "Disk size: " << disk_size << " GB";
-//     // std::cout << "Free space: " << free_space << " GB";
-//     // std::cout << "Used space: " << used_space << " GB";
-//     // std::cout << "Maximum throughput: " << max_throughput << " GB/s";
-
-//     // return 0;
-//     /* Закрываем поток */
-//     // fs::path path = fs::current_path()/"example.json";
-//     // IBuilder* bd = new Builder(path);
-//     // IAgent* agent=new CpuAgent(bd);
-//     // agent->update();
-//     // agent->update();
-//     // for (ReportComposite::Iterator iter(agent->GetReport()); !iter.isEnd(); iter.Next()) {
-//     //     auto report_i = (*iter);
-//     //     for (auto& point : report_i.first) {
-//     //         std::cout << point << std::endl;
-//     //     }
-//     //     std::cout << "value = "<<report_i.second.getValue() << std::endl;
-//     //     std::cout << "critical value = "<< report_i.second.getCriticalValue() << std::endl;
-//     //     std::cout << "Result compare = "<< report_i.second.compareIsTrue() << std::endl;
-//     // }
-
-//     // auto firstNodeLevel  = bd->getResult();
-//     // std::cout<<"Agent name : "<<bd->getAgentInfo().name_<<'\n';
-//     // std::cout<<"Agent type : "<<bd->getAgentInfo().typeAgent_<<'\n';
-//     // std::cout<<"Agent interval : "<<bd->getAgentInfo().time_seconds_<<'\n';
-//     // std::cout<<"Agent enabled : "<<bd->getAgentInfo().enabled_<<'\n';
-//     // for (ReportComposite::Iterator iter(firstNodeLevel); !iter.isEnd(); iter.Next()) {
-//     //     auto report_i = (*iter);
-//     //     for (auto& point : report_i.first) {
-//     //         std::cout << point << std::endl;
-//     //     }
-//     //     std::cout << "value = "<<report_i.second.getValue() << std::endl;
-//     //     std::cout << "critical value = "<< report_i.second.getCriticalValue() << std::endl;
-//     //     std::cout << "Result compare = "<< report_i.second.compareIsTrue() << std::endl;
-//     // }
-// }
+    // auto firstNodeLevel  = bd->getResult();
+    // std::cout<<"Agent name : "<<bd->getAgentInfo().name_<<'\n';
+    // std::cout<<"Agent type : "<<bd->getAgentInfo().typeAgent_<<'\n';
+    // std::cout<<"Agent interval : "<<bd->getAgentInfo().time_seconds_<<'\n';
+    // std::cout<<"Agent enabled : "<<bd->getAgentInfo().enabled_<<'\n';
+    // for (ReportComposite::Iterator iter(firstNodeLevel); !iter.isEnd(); iter.Next()) {
+    //     auto report_i = (*iter);
+    //     for (auto& point : report_i.first) {
+    //         std::cout << point << std::endl;
+    //     }
+    //     std::cout << "value = "<<report_i.second.getValue() << std::endl;
+    //     std::cout << "critical value = "<< report_i.second.getCriticalValue() << std::endl;
+    //     std::cout << "Result compare = "<< report_i.second.compareIsTrue() << std::endl;
+    // }
+}
--- src/model/agents/Memory/MemoryAgentMacOS.cc
+++ src/model/agents/Memory/MemoryAgentMacOS.cc
@@ -1,13 +1,13 @@
 #include <mach/mach.h>
+#include <sys/mount.h>
+#include <sys/param.h>
 #include <sys/sysctl.h>
 #include <sys/types.h>
-#include <sys/param.h>
-#include <sys/mount.h>
 #include <unistd.h>
 
+#include <cstdio>
 #include <iostream>
 #include <string>
-#include <cstdio>
 
 #include "../../AgentReportF/Builder.h"
 #include "MemoryAgent.h"
@@ -23,6 +23,9 @@ void MemoryAgent::update() {
     iter_.First();
     setRamTotal();
     setRamPercent();
+    setHardVolume();
+    setHardOps();
+    setHardThroughput();
 }
 
 void MemoryAgent::setRamTotal() {
@@ -37,55 +40,53 @@ void MemoryAgent::setRamTotal() {
 }
 
 void MemoryAgent::setRamPercent() {
-    struct task_basic_info t_info;
-    mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
-    if (task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count) != KERN_SUCCESS) {
-        std::cerr << "Error: task_info failed ";
-        return;
-    }
-    const int64_t used_phys_mem = (int64_t)t_info.resident_size;
-    int mib[2] = {CTL_HW,HW_MEMSIZE};
-    int64_t total_phys_mem;
-    size_t length = sizeof(total_phys_mem);
-    if (sysctl(mib, 2, &total_phys_mem, &length, NULL, 0) < 0) {
-        std::cerr << "Error: sysctl call failed";
-        return;
-    }
-    (*iter_).second.setValue((float)used_phys_mem / (float)total_phys_mem * 100.0), iter_.Next();
+    char command[]="echo \"scale=2; ($(/usr/bin/vm_stat | grep \"Pages active\" | awk \'{print $3}\' | sed 's/\\.//') "
+        "+ $(/usr/bin/vm_stat | grep \"Pages wired down\" | awk '{print $4}' | sed 's/\\.//')) / "
+        "$(/usr/sbin/sysctl -n hw.memsize | awk '{print $1/4096}' | sed 's/\\..*//') * 100\" | bc";
+    FILE* file = popen(command, "r");
+    if (!file) throw std::runtime_error("popen() failed!");
+    char buffer[128];
+    char* u;
+    std::fgets(buffer, 128, file);
+    (*iter_).second.setValue(strtod(buffer + 0, &u)), iter_.Next();
+    pclose(file);
 }
 
-void MemoryAgent::setHardVolume(){
-    struct statfs diskInfo;
-    unsigned long long freeSpace, totalSpace;
-    if (statfs("/", &diskInfo) == 0) {
-        freeSpace = (unsigned long long) diskInfo.f_bsize * (unsigned long long) diskInfo.f_bfree;
-        totalSpace = (unsigned long long) diskInfo.f_bsize * (unsigned long long) diskInfo.f_blocks;
-        (*iter_).second.setValue(((float)totalSpace-freeSpace)/(float)totalSpace*100.0), iter_.Next();
-    } else {
-        std::cerr << "Failed to retrieve file system information" << std::endl;
-    }
+void MemoryAgent::setHardVolume() {
+    char command[]="df -m / | awk 'NR==2{print $3}'";
+    FILE* file = popen(command, "r");
+    if (!file) throw std::runtime_error("popen() failed!");
+    char buffer[128];
+    char* u;
+    std::fgets(buffer, 128, file);
+    (*iter_).second.setValue(strtod(buffer + 0, &u)/1024.0), iter_.Next();
+    pclose(file);
 }
 
-void MemoryAgent::setHardOps(){
-    struct statfs fs_info;
-    if (statfs("/", &fs_info) != 0) {
-        std::cerr << "Error: " << strerror(errno) << std::endl;
-        return ;
-    }
-    (*iter_).second.setValue(fs_info.f_iosize / fs_info.f_bsize), iter_.Next();
+void MemoryAgent::setHardOps() {
+    char command[]="iostat -d disk0 | awk 'NR==3{print $2}'";
+    FILE* file = popen(command, "r");
+    if (!file) throw std::runtime_error("popen() failed!");
+    char buffer[128];
+    char* u;
+    std::fgets(buffer, 128, file);
+    (*iter_).second.setValue(strtod(buffer + 0, &u)), iter_.Next();
+    pclose(file);
 }
 
-void MemoryAgent::setHardThroughput(){
+void MemoryAgent::setHardThroughput() {
     struct statfs fs_info;
     if (statfs("/", &fs_info) != 0) {
         std::cerr << "Error: " << strerror(errno) << std::endl;
         return;
     }
-    double disk_size = static_cast<double>(fs_info.f_bsize * fs_info.f_blocks) / (1024 * 1024 * 1024); // размер диска в Гб
-    double free_space = static_cast<double>(fs_info.f_bsize * fs_info.f_bfree) / (1024 * 1024 * 1024); // свободное место в Гб
-    double used_space = disk_size - free_space; // занятое место в Гб
-    double io_time = 1.0 / (fs_info.f_iosize / 1000.0 / 1000.0); // время на I/O операции в секундах
-    double max_throughput = used_space / io_time; // максимальная пропускная способность в Гб/с
+    double disk_size =
+        static_cast<double>(fs_info.f_bsize * fs_info.f_blocks) / (1024 * 1024 * 1024);  // размер диска в Гб
+    double free_space = static_cast<double>(fs_info.f_bsize * fs_info.f_bfree) /
+                        (1024 * 1024 * 1024);                     // свободное место в Гб
+    double used_space = disk_size - free_space;                   // занятое место в Гб
+    double io_time = 1.0 / (fs_info.f_iosize / 1000.0 / 1000.0);  // время на I/O операции в секундах
+    double max_throughput = used_space / io_time;  // максимальная пропускная способность в Гб/с
     (*iter_).second.setValue(max_throughput), iter_.Next();
 }
 
